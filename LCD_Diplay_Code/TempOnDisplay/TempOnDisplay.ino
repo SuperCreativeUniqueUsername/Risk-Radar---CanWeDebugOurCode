c464463ea2ed2b9199a5b69413036d46af17fd94
SuperCreativeUniqueUsername <112921229+SuperCreativeUniqueUsername@users.noreply.github.com>
2023-01-07 13:25:46 -0700

    Create TempOnDisplay.ino

---
+++ LCD_Diplay_Code/TempOnDisplay/TempOnDisplay.ino
@@ -0,0 +1,38 @@
+// include the library code:
+//Test code to get things working copied from https://lastminuteengineers.com/arduino-1602-character-lcd-tutorial/
+#include <LiquidCrystal.h>
+#include <dht.h>
+
+// Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// Analog Pin A0 of Arduino is connected to DHT11 out pin
+#define dht_pin A0
+
+void setup() 
+{
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  // Clears the LCD screen
+  lcd.clear();
+  lcd.print("DHT11 Humidity & temperature Sensor\n\n");
+  delay(1000);
+}
+
+void loop() 
+{
+  // Print a message to the LCD.
+  lcd.setCursor(0, 0);
+  lcd.print("Humidity = ");
+  lcd.print(DHT.humidity);
+  lcd.print("%    ");
+
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // Print a message to the LCD.
+  lcd.print("Temperature = ");
+  lcd.print(DHT.temperature); 
+  lcd.println(" C");
+}
