0443d09447d4c9e968f432230efcf9d5831c701a
D3Zyre <onebitalpha@gmail.com>
2023-01-08 09:25:20 -0700

    added comments

--- V2_PleaseWork/V2_PleaseWork.ino
+++ V2_PleaseWork/V2_PleaseWork.ino
@@ -1,5 +1,8 @@
-#include <LiquidCrystal.h>
-#include <dht.h>
+// created by Cedric, Aiden and Griffin
+// for HackED 2023
+
+#include <LiquidCrystal.h> // for controlling the LCD
+#include <dht.h> // for getting data from the temperature/humidity sensor
 
 #define dht_pin 7
 
@@ -13,29 +16,29 @@ bool mic1_array[1000];
 int mic1_index = 0;
 
 bool get_microphone_1_boolean() {
+  // obtains the boolean value for threshold of mic 1
   bool mic1 = digitalRead(Digital_Input_mic1);
   return mic1;
 }
 
-
-int Count_array(bool array[]) {
+bool Count_array(bool array[]) {
+  // counts how many 1s are in an array of length 1000
+  // if count is greater than 10, returns 1, else 0
   int count = 0;
   for (int i = 0; i < 1000; i++) {
     if (array[i] == 1) {
       count++;
       }  
     }
-    if (count >= 10){
-        return 1;}
-    else {
-      return 0;
-    }
+    if (count >= 10) {return 1;}
+    else {return 0;}
 }
+
 void DisplayTemp(){
   // This function reads the temp
   // and displays on the LCD
-  DHT.read11(dht_pin);
-  lcd.setCursor(0, 1);
+  DHT.read11(dht_pin); // polling the sensor
+  lcd.setCursor(0, 1); // only adds second line
   lcd.print(((int)DHT.temperature)); 
   lcd.print((char)223);
   lcd.print(" C");
@@ -51,11 +54,8 @@ void DisplayTemp(){
 }
 
 void setup() {
-  
-  // put your setup code here, to run once:
   Serial.begin(9600);
 
-  
   // microphone 1
   pinMode(Digital_Input_mic1, INPUT);
   
@@ -69,29 +69,29 @@ void setup() {
   lcd.setCursor(0, 1);
   lcd.print("HackED 2023");
   delay(5000);
-  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   mic1 = get_microphone_1_boolean();
   mic1_array[mic1_index] = mic1;
+  // mic1_index goes through the entire array and loops back to the beginning once it reaches the end
   mic1_index++;
   if (mic1_index > 1000) {
     mic1_index = 0;
-    if (Count_array(mic1_array)) {
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("High Noise Risk");
-      DisplayTemp();
-      delay(4000);
+  
+  if (Count_array(mic1_array)) {
+    lcd.clear();
+    lcd.setCursor(0, 0); // only edit the first line
+    lcd.print("High Noise Risk");
+    DisplayTemp();
+    delay(4000);
       }
-    else {
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("No Noise Risk");
-      DisplayTemp();
-      delay(1500);
+  else {
+    lcd.clear();
+    lcd.setCursor(0, 0); // only edit the first line
+    lcd.print("No Noise Risk");
+    DisplayTemp();
+    delay(1500);
     }
     }
   }
