2e036ea399b83bce3c55d6c7c8d93088ecaf919e
SuperCreativeUniqueUsername <112921229+SuperCreativeUniqueUsername@users.noreply.github.com>
2023-01-08 02:24:33 -0700

    Update V2_PleaseWork.ino

--- V2_PleaseWork/V2_PleaseWork.ino
+++ V2_PleaseWork/V2_PleaseWork.ino
@@ -8,14 +8,41 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 int Digital_Input_mic1 = 8;
 bool mic1;
-bool mic1_array[1000]; // 5 seconds
+bool mic1_array[1000];
 int mic1_index = 0;
 
 bool get_microphone_1_boolean() {
   bool mic1 = digitalRead(Digital_Input_mic1);
   return mic1;
 }
-//counter insmt working
+
+
+int Count_array(bool array[]) {
+  int count = 0;
+  for (int i = 0; i < 1000; i++) {
+    if (array[i] == 1) {
+      count++;
+      }  
+    }
+    if (count >= 10){
+        return 1;}
+    else {
+      return 0;
+    }
+}
+void DisplayTemp(){
+  // This function reads the temp
+  // and displays on the LCD
+  lcd.setCursor(0, 1);
+  lcd.print((int)DHT.temperature); 
+  lcd.print(" C");
+  if ((int)DHT.temperature > 26){
+    lcd.print(" High Risk");
+  }
+  else {
+    lcd.print(" Low Risk");
+  }
+}
 
 void setup() {
   
@@ -31,8 +58,10 @@ void setup() {
   // Clears the LCD screen
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Code Starts!");
-  delay(500);
+  lcd.print("RiskRadar");
+  lcd.setCursor(0, 1);
+  lcd.print("HackED 2023");
+  delay(5000);
   
 }
 
@@ -41,24 +70,21 @@ void loop() {
   mic1 = get_microphone_1_boolean();
   mic1_array[mic1_index] = mic1;
   mic1_index++;
-  Serial.print((Count_array(mic1_array)));
-  Serial.println((mic1));
-  if (Count_array(mic1_array)) {
-    Serial.print("-----GoodJob-----");
-    delay(1000);
-  }
   if (mic1_index > 1000) {
     mic1_index = 0;
     if (Count_array(mic1_array)) {
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Danger!");
-      delay(1000);
+      lcd.print("High Noise Risk");
+      DisplayTemp();
+      delay(4000);
       }
     else {
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Good!");
+      lcd.print("No Noise Risk");
+      DisplayTemp();
+      delay(2000);
     }
     }
   }
