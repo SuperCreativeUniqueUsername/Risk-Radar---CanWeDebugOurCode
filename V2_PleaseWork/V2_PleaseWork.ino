d9aed606394a7b05819e6a57823139c63c1ebf30
SuperCreativeUniqueUsername <112921229+SuperCreativeUniqueUsername@users.noreply.github.com>
2023-01-07 23:42:45 -0700

    Create V2_PleaseWork.ino

---
+++ V2_PleaseWork/V2_PleaseWork.ino
@@ -0,0 +1,14 @@
+#include <LiquidCrystal.h>
+#include <dht.h>
+#define dht_pin A0
+
+dht DHT;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
